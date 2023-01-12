37c23118d647448d4590ca68785ae45b7229b8d9
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-01-11 23:11:59 -0300

    recoloquei o app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BarraMenu from './components/barramenu.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <BarraMenu/>
+    </div>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import BarraMenu from './components/barramenu.jsx'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BarraMenu/>
+    <App/>
   </React.StrictMode>,
 )
