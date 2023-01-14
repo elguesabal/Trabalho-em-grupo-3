40b16624cad6f0f5a99d9788277c16a00c182fde
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-01-14 17:05:15 -0300

    Add files via upload
    aki tem as requisicoes utilizando o axios e o render para fazer login

---
+++ src/components/paineis/pagina login/requisicaoLogin.js
@@ -0,0 +1,31 @@
+import url from "../../url";
+
+function login() {
+    let usuario = document.getElementById('loginome').value
+    let senha = document.getElementById('loginsenha').value
+
+    axios.get(url + 'usuario')
+    .then(resposta => {
+        const user = resposta.data.map((data) => data.login).indexOf(usuario)
+
+        if (user == -1 || resposta.data[user].senha !== senha) {
+            alert('login ou senha errada')
+            
+        } else {
+            alert('login feito com sucesso')
+
+            axios.get(url + 'dados')
+            .then(resposta => {
+                const produtos = resposta.data[user].produtos.map((produtos) => produtos.produto)
+                
+                for (let i = 0; i < produtos.length; i++) {
+                    console.log(resposta.data[user].produtos[i].produto)
+                    console.log(resposta.data[user].produtos[i].valor)
+                }
+            })
+        }
+    })
+    .catch(erro => alert('nao deu certo'))
+}
+
+export default login
