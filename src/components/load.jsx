4f4bb568197394f93713fa1fdbe56921f30b7dc5
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-01-14 23:45:20 -0300

    Add files via upload

---
+++ src/components/load.jsx
@@ -0,0 +1,17 @@
+function Load() {
+
+    return (
+        <div id="logoLoad">
+            <div class="corpoLoad">
+                <div class="containerLogo">
+                    <div class="ring"></div>
+                    <div class="ring"></div>
+                    <div class="ring"></div>
+                    <span class="loading"><strong>Carregando...</strong></span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Load
