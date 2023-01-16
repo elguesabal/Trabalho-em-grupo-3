3f6bd67f4bbc2299d6d87e19216afbf0454b63d1
erickvieira170 <114249360+erickvieira170@users.noreply.github.com>
2023-01-16 16:31:51 -0300

    Add files via upload
    adicionei o conteudo da pagina de colaboradores

--- src/components/paineis/pagina colaboradores/paginaColaboradores.jsx
+++ src/components/paineis/pagina colaboradores/paginaColaboradores.jsx
@@ -1,8 +1,124 @@
 function PaginaColaboradores() {
 
     return(
-        <div>
-            <p>colaboradores</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p>
+        <div>   
+            <div class="containerColaboradores">         
+
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/Eduarda.jpeg"/>
+                    </div>
+
+                    <div class="content">
+                        <div class="details">
+                            <h2>Eduarda Marcos<br/> <span>Gestora de Conhecimento</span></h2>
+
+                            <ul class="social_icons">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/EduardaMarcos"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/maria-eduarda-marcos-a03870252/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/José.jpeg"/>
+                    </div>
+
+                    <div class="content">
+                        <div class="details">
+                            <h2>José Antonio <br/> <span>Co-Facilitador</span></h2>
+                            <ul class="social_icons">
+                                <li><a href="https://www.facebook.com/joseantonio.elguesabal.7?mibextid=ZbWKwL"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/elguesabal"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/jos%C3%A9-antonio-b14160240/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/Bruno.jfif"/>
+                    </div>
+
+                    <div class="content">
+                        <div class="details">
+                            <h2>Bruno Pereira<br/> <span>Gestor de Gente & Engajamento</span></h2>
+                            <ul class="social_icons">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/Bryhnu"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/bruno-pereira-50346222a/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+        
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/Leonardo.jpeg"/>
+                    </div>
+
+                    <div class="content">
+                        <div class="details">
+                            <h2>Leonardo Villar <br/> <span>Colaborador</span></h2>
+                            <ul class="social_icons">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/leovillar94"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/leonardo-villar-0393a2123/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/Diego.jpeg"/>
+                    </div>
+        
+                    <div class="content">
+                        <div class="details">
+                            <h2>Diego bezerra <br/> <span>Colaborador</span></h2>
+                            <ul class="social_icons">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/diegomars1"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/diego-pereira-340191262/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="https://www.instagram.com/diegosilvamars/?igshid=YmMyMTA2M2Y%3D"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="cardsColaboradores">
+                    <div class="imgBx">
+                        <img src="./img/Erick.jpeg"/>
+                    </div>
+
+                    <div class="content">
+                        <div class="details">
+                            <h2>Erick Vieira <br/> <span>Colaborador</span></h2>
+                            <ul class="social_icons">
+                                <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                <li><a href="https://github.com/erickvieira170"><ion-icon name="logo-github"></ion-icon></a></li>
+                                <li><a href="https://www.linkedin.com/in/erick-vieira-85649b252/"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                                <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     )
 }
