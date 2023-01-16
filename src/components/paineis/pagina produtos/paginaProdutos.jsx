bd93cafd35714f71fb4672b306a7ff954caf6110
leovillar94 <114249176+leovillar94@users.noreply.github.com>
2023-01-16 17:18:18 -0300

    Add files via upload
    colocando os cards dos produtos

--- src/components/paineis/pagina produtos/paginaProdutos.jsx
+++ src/components/paineis/pagina produtos/paginaProdutos.jsx
@@ -1,9 +1,309 @@
 function PaginaProdutos() {
 
     return (
-        <div>
-            <p>card com produtos</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p>
+        <div id="grid">
+
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="card">
+                <div class="lines"></div>
+                <div class="imgBx">
+                    <img src=""/>
+                </div>
+                <div class="content">
+                    <div class="details">
+                        <h2 class="nomeProduto"></h2>
+                        <p class="descricaoProduo">deveria ter um texto aki so pra fica como exemplo mas eu to com preguica de procurar um aleatorio entao vai ficar esse aki sem sentido mesmo</p>
+
+                        <div class="aaaaaa">
+                            <label for="estoque" class="estoque">Estoque Atual:</label>
+                            <input type="text" class="quantidadeEstoque" name="estoque"/>
+                        </div>
+
+                        <div class="aaaaaa">
+                            <label for="">Adicionar:</label>
+                            <input type="number" class="numerosItens"/>
+                        </div>
+
+                        <button class="enviarItens">Enviar</button>
+
+                    </div>
+                </div>
+
+            </div>
+
+            
         </div>
+
     )
 }
 
