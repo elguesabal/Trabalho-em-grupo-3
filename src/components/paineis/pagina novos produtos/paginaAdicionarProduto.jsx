09f4eead7defa21f808e99fd157f285480145c41
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-01-17 16:51:47 -0300

    Add files via upload
    adicionei onclick={() => adicionarProduto()}

--- src/components/paineis/pagina novos produtos/paginaAdicionarProduto.jsx
+++ src/components/paineis/pagina novos produtos/paginaAdicionarProduto.jsx
@@ -2,45 +2,7 @@ function PaginaAdicionarProduto() {
 
     return(
         <div>
-
-             <div class="corpo" id="corpo1">
-                <div class="box">
-                    <div class="form">
-                        <h2>Novo produto</h2>
-
-                        <div class="inputBox">
-                            <span>Nome do produto</span>
-                            <input id="nomeproduto" type="text" required="required"></input>
-                            <i></i>
-                        </div>
-
-                        <div class="inputBox">
-                            <span>Quantidade</span>
-                            <input
-                                id="quantidadeproduto"
-                                type="number"
-                                min="0"
-                                required="required"
-                            ></input>
-                            <i></i>
-                        </div>
-                        <div class="inputBox">
-                            <span>Link da imagem </span>
-                            <input
-                                id="imagemlink"
-                                type="text"
-                                required="required"
-                            ></input>
-                            <i></i>
-                        </div>
-
-                        <button id="botaologin" class="botão">
-                            Criar
-                        </button>
-                    </div>
-                </div>
-            </div>
-
+            <p>novos produtos</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p>
         </div>
     )
 }
