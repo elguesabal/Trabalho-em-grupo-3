3fd9fd8ca823a46a6aac299efeb89c7ceac8100d
elguesabal <113626409+elguesabal@users.noreply.github.com>
2023-01-17 16:54:46 -0300

    Add files via upload
    adicionado evento de click no jsx e adicionado a funcao no js

--- src/components/paineis/pagina novos produtos/paginaAdicionarProduto.jsx
+++ src/components/paineis/pagina novos produtos/paginaAdicionarProduto.jsx
@@ -1,8 +1,48 @@
+import adicionarProduto from "./requisicaoAdicionarProduto"
+
 function PaginaAdicionarProduto() {
 
     return(
         <div>
-            <p>novos produtos</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p>
+
+             <div class="corpo" id="corpo1">
+                <div class="box">
+                    <div class="form">
+                        <h2>Novo produto</h2>
+
+                        <div class="inputBox">
+                            <span>Nome do produto</span>
+                            <input id="nomeproduto" type="text" required="required"></input>
+                            <i></i>
+                        </div>
+
+                        <div class="inputBox">
+                            <span>Quantidade</span>
+                            <input
+                                id="quantidadeproduto"
+                                type="number"
+                                min="0"
+                                required="required"
+                            ></input>
+                            <i></i>
+                        </div>
+                        <div class="inputBox">
+                            <span>Link da imagem </span>
+                            <input
+                                id="imagemlink"
+                                type="text"
+                                required="required"
+                            ></input>
+                            <i></i>
+                        </div>
+
+                        <button id="botaologin" class="botão" onClick={() => adicionarProduto()}>
+                            Criar
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
---
+++ src/components/paineis/pagina novos produtos/requisicaoAdicionarProduto.js
@@ -0,0 +1,36 @@
+import url from "../../url";
+
+
+async function adicionarProduto() {
+    let usuario = document.getElementById('loginome').value
+
+    axios.get(url + 'usuario')
+    .then(resposta => {
+        const user = resposta.data.map((data) => data.login).indexOf(usuario)
+        const userId = resposta.data[user].id
+
+        axios.get(url + 'dados')
+        .then(resposta => {
+            let arrayProdutos = resposta.data[user].produtos
+
+            let produto = document.getElementById('nomeproduto').value
+            let quantidade = document.getElementById('quantidadeproduto').value
+            let imagem = document.getElementById('imagemlink').value
+
+            arrayProdutos.push({ produto: produto, quantidade: quantidade, imagem: imagem})
+
+            let arrayFinal = {
+                            id: user,
+                            produtos: arrayProdutos
+                        }
+
+            axios.put(url + 'dados/' + userId, arrayFinal)
+            .then(resposta => alert('item adicionado'))
+            .catch(erro => alert(erro))
+
+        })
+    })
+    .catch(erro => alert(erro))
+}
+
+export default adicionarProduto
