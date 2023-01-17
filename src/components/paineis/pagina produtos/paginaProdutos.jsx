95be71f08bd1f2ca805583527ef86fd3ba0b4f0a
Bruno Pereira <brunocarvpinto@gmail.com>
2023-01-17 17:26:03 -0300

    Add files via upload
    Atualizando os cards

--- src/components/paineis/pagina produtos/paginaProdutos.jsx
+++ src/components/paineis/pagina produtos/paginaProdutos.jsx
@@ -1,28 +1,302 @@
 function PaginaProdutos() {
 
     return (
-        <div id="grid">
 
-            {/* <div class="card">
-                <div class="lines"></div>
-                <div class="imgBx">
-                    <img src=""/>
-                </div>
-                <div class="content">
-                    <div class="details">
-                        <div class="estoque">
-                            <h2>Design</h2>
-                            <p>Estoque: 10</p>
+        <div id="gridProdutos">
+
+            <div class="containerProdutos">
+
+                <div class="cardProdutos">
+
+                    <div class="face face1">
+                        <div class="content">
+                            <img src="design.png" />
+                            <h3>Exemplo</h3>
                         </div>
-                        <br/>
-                        <input type="number" min="0"/>
-                        
-                        <a href="#">Atualizar quantidade</a>
-                        <ion-icon class="lixo" name="trash-outline"></ion-icon>
+                    </div>
+
+                    <div class="face face2">
+                        <div class="content">
+
+                            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                            <div class="estoqueAtual">
+                                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                                <p class="moverP" name="estoque">exemplo</p>
+                            </div>
+
+                            <div class="botoesReq">
+                                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                                <input type="number" class="inputNumb" name="atualizar" />
+                            </div>
+
+                            <div>
+                                <button>Atualizar</button>
+                                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+                            </div>
+
+                        </div>
+
                     </div>
                 </div>
-            </div> */}
-            
+
+            </div>
+
+            <div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+<div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+<div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+<div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+
+<div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+<div class="containerProdutos">
+
+<div class="cardProdutos">
+
+    <div class="face face1">
+        <div class="content">
+            <img src="design.png" />
+            <h3>Exemplo</h3>
+        </div>
+    </div>
+
+    <div class="face face2">
+        <div class="content">
+
+            <p class="descricaoProdutos">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua.</p>
+
+            <div class="estoqueAtual">
+                <label for="estoque" class="estilizarLabel">Estoque Atual:</label>
+                <p class="moverP" name="estoque">exemplo</p>
+            </div>
+
+            <div class="botoesReq">
+                <label for="atuzaliar" class="estilizarLabel">Atualizar Produtos:</label>
+                <input type="number" class="inputNumb" name="atualizar" />
+            </div>
+
+            <div>
+                <button>Atualizar</button>
+                <ion-icon class="lixeira" name="trash-outline" title="Excluir Produto"></ion-icon>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</div>
+
+
         </div>
     )
 }
