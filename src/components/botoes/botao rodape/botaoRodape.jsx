af8b49e9e57f51a637c4ac3246c78ae9bc1b4cd8
Maria Eduarda <114101867+EduardaMarcos@users.noreply.github.com>
2023-01-18 13:33:44 -0300

    Add files via upload
    troca de icons

--- src/components/botoes/botao rodape/botaoRodape.jsx
+++ src/components/botoes/botao rodape/botaoRodape.jsx
@@ -5,8 +5,8 @@ function BotaoRodape() {
     return(
         <li class="list" id="corNav5" onClick={() => meteNoneBlock('n5')}>
             <a href="#">
-                <span class="icon"><ion-icon name="settings-outline"></ion-icon></span>
-                <span class="text">Settings</span>
+                <span class="icon"><ion-icon name="chatbubbles-outline"></ion-icon></span>
+                <span class="text">Informações</span>
             </a>
         </li>
     )
