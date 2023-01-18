e6ca6d73ceda747915f88b5b38507c8ac625ed37
Maria Eduarda <114101867+EduardaMarcos@users.noreply.github.com>
2023-01-18 13:32:21 -0300

    Add files via upload

--- src/components/botoes/botao pagina de produtos/botaoPaginaProdutos.jsx
+++ src/components/botoes/botao pagina de produtos/botaoPaginaProdutos.jsx
@@ -5,8 +5,8 @@ function BotaoProdutos() {
     return(
         <li class="list" id="corNav2" onClick={() => meteNoneBlock('n2')}>
             <a href="#">
-                <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
-                <span class="text">About</span>
+                <span class="icon"><ion-icon name="eye-outline"></ion-icon></span>
+                <span class="text">Visualizar</span>
             </a>
         </li>
     )
