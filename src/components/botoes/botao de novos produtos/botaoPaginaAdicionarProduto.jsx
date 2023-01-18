d65255ac70623e58e7fe635050fa6b6fce41675d
Maria Eduarda <114101867+EduardaMarcos@users.noreply.github.com>
2023-01-18 12:44:08 -0300

    Add files via upload

--- src/components/botoes/botao de novos produtos/botaoPaginaAdicionarProduto.jsx
+++ src/components/botoes/botao de novos produtos/botaoPaginaAdicionarProduto.jsx
@@ -5,8 +5,8 @@ function BotaoPaginaAdicionarProduto() {
     return(
         <li class="list" id="corNav3" onClick={() => meteNoneBlock('n3')}>
             <a href="#">
-                <span class="icon"><ion-icon name="chatbubble-outline"></ion-icon></span>
-                <span class="text">Messages</span>
+                <span class="icon"><ion-icon name="duplicate-outline"></ion-icon></span>
+                <span class="text">Adicionar</span>
             </a>
         </li>
     )
