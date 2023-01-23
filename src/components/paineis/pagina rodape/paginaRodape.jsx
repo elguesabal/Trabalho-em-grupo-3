a11da2db67e5f1b9f7bf61f9841ea3f040db903e
Maria Eduarda <114101867+EduardaMarcos@users.noreply.github.com>
2023-01-23 16:23:21 -0300

    Add files via upload

--- src/components/paineis/pagina rodape/paginaRodape.jsx
+++ src/components/paineis/pagina rodape/paginaRodape.jsx
@@ -2,7 +2,32 @@ function PaginaRodape() {
 
     return (
         <div>
-            <p>roda pe</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p><p>sadf</p>
+            
+            <div class="corpo" id="corpo1">
+                <div class="box4">
+                    <div class="form">
+                        <h2>Gostaria de enviar um feedback?</h2>
+
+                        <div class="inputBox">
+                            <span>Digite seu email:</span>
+                            <input id="loginome" type="text" required="required"></input>
+                            <i></i>
+                        </div>
+
+                        <div class="inputBox">
+                            <textarea placeholder="digite sua mensagem..." class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <button  class="botão2" >
+                        Enviar
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="canva">
+  <iframe loading="lazy" class="canva2"
+    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFYgvHt1lE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+  </iframe>
+</div>
         </div>
     )
 }
