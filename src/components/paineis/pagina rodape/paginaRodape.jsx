766e1419aab8e68cb08b1a0d19e7776e0f55698e
leovillar94 <114249176+leovillar94@users.noreply.github.com>
2023-01-23 17:52:11 -0300

    Add files via upload
    adicionei os botões das redes sociais

--- src/components/paineis/pagina rodape/paginaRodape.jsx
+++ src/components/paineis/pagina rodape/paginaRodape.jsx
@@ -3,7 +3,7 @@ function PaginaRodape() {
     return (
         <div>
             
-            <div class="corpo" id="corpo1">
+            <div class="corpo" id="corpoRodape">
                 <div class="box4">
                     <div class="form">
                         <h2>Gostaria de enviar um feedback?</h2>
@@ -23,12 +23,42 @@ function PaginaRodape() {
                     </div>
                 </div>
             </div>
+
             <div class="canva">
-  <iframe loading="lazy" class="canva2"
-    src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFYgvHt1lE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
-  </iframe>
-</div>
+                <iframe loading="lazy" class="canva2"
+                src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAFYgvHt1lE&#x2F;view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
+                </iframe>
+            </div>
+
+
+            <div id="redesSocias">
+                <a class="redesSocias" href="https://facebook.com/" target="blank"><ion-icon class="iconRedeSocial" name="logo-facebook"></ion-icon></a>
+                
+                <a class="redesSocias" href ="https://twitter.com/" target="blank"><ion-icon class="iconRedeSocial" name="logo-twitter"></ion-icon></a>
+
+                <a class="redesSocias" href ="https://www.youtube.com/" target="blank"><ion-icon class="iconRedeSocial" name="logo-youtube"></ion-icon></a>
+                
+                <a class="redesSocias" href ="https://www.instagram.com/" target="blank"><ion-icon class="iconRedeSocial" name="logo-instagram"></ion-icon></a>
+                
+                <a class="redesSocias" href ="https://br.linkedin.com/" target="blank"><ion-icon class="iconRedeSocial" name="logo-linkedin"></ion-icon> </a>
+            </div>
+              
+            <div>
+                
+                
+                <h1>Institucional</h1> 
+                
+                <p>Trabalhe Conosco</p>
+
+                <p>Aplicativos</p>
+
+                <p>Canal De Ouvidoria</p>
+
+                <p>Venda No Marketplace</p>
+            </div>
+
         </div>
+        
     )
 }
 
